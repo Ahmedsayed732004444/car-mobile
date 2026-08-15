fbf9ea9417e3159f13e00f1e97a1d2a12631109a
Ahmed Sayed <ahmedsayed@gmail.com>
2026-08-15 17:55:55 +0300

    Hide hintText placeholder immediately upon focus in CustomTextField

--- lib/widgets/custom_textfield.dart
+++ lib/widgets/custom_textfield.dart
@@ -5,7 +5,7 @@ import '../core/utils/size_config.dart';
 
 enum CustomBorderType { outline, underline }
 
-class CustomTextField extends StatelessWidget {
+class CustomTextField extends StatefulWidget {
   const CustomTextField({
     Key? key,
     required this.label,
@@ -25,7 +25,10 @@ class CustomTextField extends StatelessWidget {
     this.readOnly = false,
     this.counterText,
     this.textDirection,
-    this.borderType = CustomBorderType.outline, this.onTap, this.fillColor, this.filled = false,
+    this.borderType = CustomBorderType.outline,
+    this.onTap,
+    this.fillColor,
+    this.filled = false,
   }) : super(key: key);
 
   final String label;
@@ -50,40 +53,95 @@ class CustomTextField extends StatelessWidget {
   final Color? fillColor;
   final bool? filled;
 
+  @override
+  State<CustomTextField> createState() => _CustomTextFieldState();
+}
+
+class _CustomTextFieldState extends State<CustomTextField> {
+  late FocusNode _effectiveFocusNode;
+  bool _isCreatedInternalFocus = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _initFocusNode();
+  }
+
+  void _initFocusNode() {
+    if (widget.focusNode != null) {
+      _effectiveFocusNode = widget.focusNode!;
+      _isCreatedInternalFocus = false;
+    } else {
+      _effectiveFocusNode = FocusNode();
+      _isCreatedInternalFocus = true;
+    }
+    _effectiveFocusNode.addListener(_handleFocusChange);
+  }
+
+  @override
+  void didUpdateWidget(CustomTextField oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (widget.focusNode != oldWidget.focusNode) {
+      _effectiveFocusNode.removeListener(_handleFocusChange);
+      if (_isCreatedInternalFocus) {
+        _effectiveFocusNode.dispose();
+      }
+      _initFocusNode();
+    }
+  }
+
+  void _handleFocusChange() {
+    if (mounted) {
+      setState(() {});
+    }
+  }
+
+  @override
+  void dispose() {
+    _effectiveFocusNode.removeListener(_handleFocusChange);
+    if (_isCreatedInternalFocus) {
+      _effectiveFocusNode.dispose();
+    }
+    super.dispose();
+  }
+
   InputBorder _border({required Color color}) {
     final borderRadius = BorderRadius.circular(12);
 
-    return borderType == CustomBorderType.outline
+    return widget.borderType == CustomBorderType.outline
         ? OutlineInputBorder(
-      borderRadius: borderRadius,
-      borderSide: BorderSide(color: color),
-    )
+            borderRadius: borderRadius,
+            borderSide: BorderSide(color: color),
+          )
         : UnderlineInputBorder(
-      borderSide: BorderSide(color: color),
-    );
+            borderSide: BorderSide(color: color),
+          );
   }
 
   @override
   Widget build(BuildContext context) {
+    // Hide hint immediately when field is focused OR when text is present!
+    final showHint = !_effectiveFocusNode.hasFocus && widget.hint.isNotEmpty;
+
     return TextFormField(
-      keyboardType: keyboardType,
-      controller: controller,
-      obscureText: obscure,
-      readOnly: readOnly,
-      textDirection: textDirection,
-      onTap: onTap,
+      keyboardType: widget.keyboardType,
+      controller: widget.controller,
+      obscureText: widget.obscure,
+      readOnly: widget.readOnly,
+      textDirection: widget.textDirection,
+      onTap: widget.onTap,
       style: TextStyle(
         color: AppColor.blackColor,
         fontSize: SizeConfig.widthResponsive(0.04),
         fontWeight: FontWeight.w500,
       ),
-      textInputAction: textInputAction,
+      textInputAction: widget.textInputAction,
       decoration: InputDecoration(
         contentPadding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-        labelText: label.isNotEmpty ? label : null,
-        hintText: hint.isNotEmpty ? hint : null,
-        fillColor: fillColor,
-        filled: filled,
+        labelText: widget.label.isNotEmpty ? widget.label : null,
+        hintText: showHint ? widget.hint : null,
+        fillColor: widget.fillColor,
+        filled: widget.filled,
         labelStyle: TextStyle(
           color: AppColor.greyShade2,
           fontSize: SizeConfig.widthResponsive(0.04),
@@ -98,16 +156,16 @@ class CustomTextField extends StatelessWidget {
         errorBorder: _border(color: Colors.red),
         focusedErrorBorder: _border(color: AppColor.primaryColor),
         focusedBorder: _border(color: AppColor.primaryColor),
-        prefixIcon: prefixIcon,
-        suffixIcon: suffixIcon,
-        counterText: counterText,
+        prefixIcon: widget.prefixIcon,
+        suffixIcon: widget.suffixIcon,
+        counterText: widget.counterText,
       ),
-      onFieldSubmitted: onFieldSubmitted,
-      validator: validator,
-      onSaved: onSaved,
-      focusNode: focusNode,
-      maxLines: maxLines,
-      maxLength: maxLength,
+      onFieldSubmitted: widget.onFieldSubmitted,
+      validator: widget.validator,
+      onSaved: widget.onSaved,
+      focusNode: _effectiveFocusNode,
+      maxLines: widget.maxLines,
+      maxLength: widget.maxLength,
     );
   }
 }
